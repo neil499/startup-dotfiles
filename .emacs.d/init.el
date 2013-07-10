1e0ee938dc47383b1916b12693c57bb69b2d791a
neil499 <neil499@startup.(none)>
2013-07-10 15:02:56 -0400

    fixing backspace on my virtual machine - not EC2

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -75,3 +75,6 @@
 (require 'jade-mode)    
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+;; ----
+(normal-erase-is-backspace-mode 0)
